c90480d191e54f3b2b0e314f3e74238ee17075c0
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-19 15:54:59 +0300

    last updates

--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -43,6 +43,11 @@ export const SearchWrapper = styled.div`
       border-radius: 5px;
       outline: none;
       box-shadow: rgba(0, 0, 0, 0.4) 0px 3px 10px;
+      &:focus-within {
+        /* border-bottom: 1px solid #9eb7e5; */
+        box-shadow: 0 3px 15px rgb(43, 46, 48);
+        border-radius: 5px;
+      }
     }
   }
 `;
